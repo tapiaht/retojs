aca44994ff3559f428c4968221b985b90c8ae37c
tapiaht <hiver.tapia@uab.edu.bo>
2023-04-02 23:50:53 -0400

    reto promise then

--- promesas.js
+++ promesas.js
@@ -17,17 +17,16 @@ import { doTask1, doTask2, doTask3 } from './tasks.mjs';
  */
 function runCode() {
     const strings = [];
-    return new Promise((resolve) 
-      .then(doTask1((rta1) => {
-        strings.push(rta1)}))
-      .then(doTask2((rta2) => {
-          strings.push(rta2)}))
-      .then(doTask3((rta3) => {
-            strings.push(rta3)
-            resolve(strings)
-          }))
-        .catch()
-    )
+    return doTask1()
+      .then(function(rta1){
+        strings.push(rta1);
+        return doTask2()})
+      .then(function(rta2){
+        strings.push(rta2)
+        return doTask3()})
+      .then(function(rta3){
+        strings.push(rta3)
+        return strings})
 }
 
 
--- tasks.mjs
+++ tasks.mjs
@@ -1,11 +1,17 @@
- export function doTask1(callback) {
-    setTimeout(() => callback('Task 1'), 300);
+ export function doTask1() {
+    return new Promise((resolve)=>{
+    setTimeout(() => resolve('Task 1'), 300);
+  }) 
   }
   
- export  function doTask2(callback) {
-    setTimeout(() => callback('Task 2'), 300);
+ export  function doTask2() {
+    return new Promise((resolve)=>{
+      setTimeout(() => resolve('Task 2'), 300);
+    })
   }
   
- export  function doTask3(callback) {
-    setTimeout(() => callback('Task 3'), 300);
+ export  function doTask3() {
+    return new Promise((resolve)=>{
+      setTimeout(() => resolve('Task 3'), 300);
+    })
   }
